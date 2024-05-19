45cb18c030e38f645a1205a19f329acdce2ce985
aetrig <emilia.gosztyla@gmail.com>
2024-05-19 22:35:38 +0200

    Full riot acc done (with summoners)

--- main.go
+++ main.go
@@ -14,14 +14,16 @@ import (
 var api_token string
 
 type RiotAcc struct {
-	Puuid    string
-	GameName string
-	TagLine  string
+	GameName  string
+	TagLine   string
+	Puuid     string
+	Id        string
+	AccountId string
 }
 
-// Return empty struct if Status != 200 OK
-func getRiotAcc(gameName string, tagLine string) RiotAcc {
-	var Acc RiotAcc
+// Doesn't modify RiotAcc if Status != 200 OK
+func getRiotAccByGameNameTagLine(riotAcc *RiotAcc, gameName string, tagLine string) {
+	//var Acc RiotAcc
 	link := fmt.Sprintf("https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s", gameName, tagLine, api_token)
 	resp, err := http.Get(link)
 	if err != nil {
@@ -33,19 +35,36 @@ func getRiotAcc(gameName string, tagLine string) RiotAcc {
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = json.Unmarshal(JSONBody, &Acc)
+		err = json.Unmarshal(JSONBody, &riotAcc)
 		if err != nil {
 			log.Fatal(err)
 		}
-		return Acc
 	}
 	fmt.Println(resp.Status)
-	return RiotAcc{}
+	link = fmt.Sprintf("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/%s?api_key=%s", riotAcc.Puuid, api_token)
+	resp, err = http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		JSONBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.Unmarshal(JSONBody, &riotAcc)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println(resp.Status)
 }
 
 func main() {
 	godotenv.Load()
 	api_token = os.Getenv("RIOT_TOKEN")
-	aetrig := getRiotAcc("aetrig", "uwu")
-	fmt.Printf("puuid: %s\ngameName: %s\ntagLine: %s\n", aetrig.Puuid, aetrig.GameName, aetrig.TagLine)
+	var aetrig RiotAcc
+	getRiotAccByGameNameTagLine(&aetrig, "aetrig", "uwu")
+	fmt.Println(aetrig)
+	//fmt.Printf("puuid: %s\ngameName: %s\ntagLine: %s\n", aetrig.Puuid, aetrig.GameName, aetrig.TagLine)
 }
