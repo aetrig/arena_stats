2251ad67725959e6cda2cbdbfe7a64f64d67ef7d
aetrig <emilia.gosztyla@gmail.com>
2024-05-20 16:39:05 +0200

    reading specific match data from JSON

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	//"fmt"
+
 	"fmt"
 
 	"github.com/joho/godotenv"
@@ -14,10 +16,11 @@ func main() {
 	GetRiotAccByGameNameTagLine(&aetrig, "aetrig", "uwu", Eu_west)
 	GetRiotAccByGameNameTagLine(&sunny, "SunnyAsh", "AETI", Eu_west)
 	GetRiotAccByGameNameTagLine(&ragna, "abs woman", "1312", Eu_west)
-	aetrig.Print()
-	sunny.Print()
-	ragna.Print()
-	var matches []string = GetMatchesByPUUID(aetrig.Puuid)
-	fmt.Println(matches)
-	WriteToFile(matches, "test.txt")
+	// aetrig.Print()
+	// sunny.Print()
+	// ragna.Print()
+	var matches []string = GetMatchesByRiotAcc(aetrig)
+	WriteToFile(matches, "matches.txt")
+	match := GetMatchByID(matches[0], aetrig)
+	fmt.Printf("ID: %s\nChampion: %s\nPlacement: %d\n", match.MatchID, match.Champion, match.Placement)
 }
--- matchIDs.go
+++ matchIDs.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	//"reflect"
 )
 
@@ -14,7 +15,33 @@ const April20 string = "1713618542"
 const ArenaID string = "1700"
 const n_matches string = "100"
 
-func GetMatchesByPUUID(puuid string) []string {
+type _Match struct {
+	MatchID   string
+	Champion  string
+	Placement int
+	//PlayedWithSunny bool
+}
+
+type _Metadata struct {
+	Participants []string `json:"participants"`
+}
+
+type _Participant struct {
+	ChampionName string `json:"championName"`
+	Placement    int    `json:"placement"`
+}
+
+type _Info struct {
+	Participants []_Participant `json:"participants"`
+}
+
+type _MatchJSON struct {
+	Metadata _Metadata `json:"metadata"`
+	Info     _Info     `json:"info"`
+}
+
+func GetMatchesByRiotAcc(riotAcc RiotAcc) []string {
+	puuid := riotAcc.Puuid
 	var matches []string
 	var api_token string = os.Getenv("RIOT_TOKEN")
 	link := fmt.Sprintf("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/%s/ids?startTime=%s&queue=%s&start=0&count=%s&api_key=%s", puuid, April20, ArenaID, n_matches, api_token)
@@ -38,16 +65,44 @@ func GetMatchesByPUUID(puuid string) []string {
 	return matches
 }
 
-func WriteToFile(arrayToWrite []string, fileName string) {
-	for i, j := 0, len(arrayToWrite)-1; i < j; i, j = i+1, j-1 {
-		arrayToWrite[i], arrayToWrite[j] = arrayToWrite[j], arrayToWrite[i]
-	}
-	var writeToFile string = ""
-	for i := 0; i < len(arrayToWrite); i++ {
-		writeToFile += arrayToWrite[i] + "\n"
-	}
-	err := os.WriteFile(fileName, []byte(writeToFile), 0666)
+func WriteToFile(data []string, fileName string) {
+	content := strings.Join(data, "\n") + "\n"
+	err := os.WriteFile(fileName, []byte(content), 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func GetMatchByID(matchID string, acc RiotAcc) _Match {
+	var match _Match
+	var matchJSON _MatchJSON
+	var api_token string = os.Getenv("RIOT_TOKEN")
+	link := fmt.Sprintf("https://europe.api.riotgames.com/lol/match/v5/matches/%s?api_key=%s", matchID, api_token)
+	resp, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		JSONBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.Unmarshal(JSONBody, &matchJSON)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println(resp.Status)
+	}
+	var accIndex int = -1
+	for i := 0; i < len(matchJSON.Metadata.Participants); i++ {
+		if acc.Puuid == matchJSON.Metadata.Participants[i] {
+			accIndex = i
+		}
+	}
+	match.MatchID = matchID
+	match.Champion = matchJSON.Info.Participants[accIndex].ChampionName
+	match.Placement = matchJSON.Info.Participants[accIndex].Placement
+	return match
 }
