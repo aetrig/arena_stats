e5cacad1bc3f336e97647ef4d9c86a8520dd1b14
aetrig <emilia.gosztyla@gmail.com>
2024-07-03 15:27:13 +0200

    organazing main a lil bit

--- main.go
+++ main.go
@@ -6,32 +6,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func prepareMatchHistory(name string, tag string, server Server) {
+	var acc RiotAcc
+	GetRiotAccByGameNameTagLine(&acc, name, tag, server)
+	WriteToMatchesFile(GetMatchesByRiotAcc(acc), acc)
+	CreateMatchHistoryFile(acc)
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
-	var aetrig RiotAcc
-	// var sunny RiotAcc
-	// var ragna RiotAcc
-	GetRiotAccByGameNameTagLine(&aetrig, "aetrig", "uwu", Eu_west)
-	//GetRiotAccByGameNameTagLine(&sunny, "SunnyAsh", "AETI", Eu_west)
-	//GetRiotAccByGameNameTagLine(&ragna, "abs woman", "1312", Eu_west)
-	// aetrig.Print()
-	// sunny.Print()
-	// ragna.Print()
-	var last100Matches []string = GetMatchesByRiotAcc(aetrig)
-	WriteToMatchesFile(last100Matches, aetrig)
-	// start := time.Now()
-	// match := GetMatchByID(last100Matches[2], aetrig)
-	// end := time.Since(start)
-	// fmt.Println(end)
-	// fmt.Printf(
-	// 	"Newest match:\nID: %s\nChampion: %s\nPlacement: %d\n",
-	// 	match.MatchID,
-	// 	match.Champion,
-	// 	match.Placement,
-	// )
-	// WriteToStatsFile(match)
-	CreateMatchHistoryFile(aetrig)
+	prepareMatchHistory("aetrig", "uwu", Eu_west)
 }
